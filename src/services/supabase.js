11e7b983265960a61dd81e2bcd7dcca645c09a5e
Danny Garcia <dgarcia1724@berkeley.edu>
2024-03-07 02:59:11 -0800

    345. Setting Up Storage Buckets

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://nemgpmyddbhgqqzgrapt.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lbWdwbXlkZGJoZ3FxemdyYXB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk3NzE4ODcsImV4cCI6MjAyNTM0Nzg4N30.oY5eeY2-VhFX8FCWVQIam29eyWTSpWmFQqmJKnSe2nM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
