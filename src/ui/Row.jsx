2ff726e7f07e8836f20f432a27627eff2e031b35
Danny Garcia <dgarcia1724@berkeley.edu>
2024-03-08 20:52:25 -0800

    357. Abstracting React Query Into Custom Hooks

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -4,7 +4,7 @@ const Row = styled.div`
   display: flex;
 
   ${(props) =>
-    props.type === "horizantal" &&
+    props.type === "horizontal" &&
     css`
       justify-content: space-between;
       align-items: center;
